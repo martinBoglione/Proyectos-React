ad4fe9d5e558eec51eafc37a227ea6be41d6e9f6
Martin <martinboglione97@gmail.com>
2023-09-11 16:08:42 -0300

    button-component

---
+++ button-component/src/App.js
@@ -0,0 +1,14 @@
+import Footer from './components/footer.js'
+
+function App() {
+  return (
+    <section className="flex flex-col flex-grow h-screen">
+      <h1 className="text-3xl font-bold underline text-red-900">
+        Hello world!
+      </h1>
+      <Footer/>
+    </section>
+  );
+}
+
+export default App;
