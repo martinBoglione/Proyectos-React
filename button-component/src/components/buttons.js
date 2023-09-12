a0b967011abd44be26cadf5b2009d6b8af8eddf2
Martin <martinboglione97@gmail.com>
2023-09-12 11:52:46 -0300

    buttons on the second line

--- button-component/src/components/buttons.js
+++ button-component/src/components/buttons.js
@@ -5,19 +5,28 @@ function Buttons() {
     <section className='p-10 grow text-black'>
       <header className='text-tittle-buttons text-[24px] mb-8 mt-3'>Buttons</header>
 
-      <aricle className='flex flex-row text-[10px]' >
-        <div className='mr-40'>
-          <div> &lt;Button /&gt; </div>
-          <button class="bg-gray-200 py-2 px-4 rounded mt-2">Button</button>
+      <aricle className='flex flex-row w-[300px] justify-between text-[10px] mb-10' >
+        <div>
+          <div> &lt;Default /&gt; </div>
+          <button class="bg-gray-200 font-bold py-2 px-4 rounded mt-2">Default</button>
         </div>
         <div>
           <div className='text-gray-400'> &:hover, &:focus </div>
-          <button class="bg-gray-300 focus:ring focus:ring-black hover:bg-gray-200 py-2 px-4 rounded mt-2">Button</button>
+          <button class="bg-gray-300 font-bold focus:ring focus:ring-black hover:bg-gray-200 py-2 px-4 rounded mt-2">Default</button>
+        </div>
+      </aricle>
+
+      <aricle className='flex flex-row w-[300px] justify-between text-[10px]' >
+        <div>
+          <div> &lt;Button variant="outline"/&gt; </div>
+          <button class="bg-transparent text-blue-500 border border-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+        <div>
+          <div className='text-gray-400'> &:hover, &:focus </div>
+          <button class="bg-blue-100 focus:ring focus:ring-blue hover:bg-blue-300 border border-blue-500 text-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
         </div>
       </aricle>
  
-
-
 
       <article className='m-4 text-footer font-semibold text-[12px]'>Icons: <a href='https://material.io/resources/icons/?style=round'>https://material.io/resources/icons/?style=round</a></article> 
       
