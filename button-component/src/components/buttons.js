17091100c182aca1fcfbe0dac96848f3b4b27fbe
Martin <martinboglione97@gmail.com>
2023-09-13 18:46:53 -0300

    Basic buttons template

--- button-component/src/components/buttons.js
+++ button-component/src/components/buttons.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Buttons() {
   return(
-    <section className='p-10 grow text-black'>
-      <header className='text-tittle-buttons text-[24px] mb-8 mt-3'>Buttons</header>
+    <section className='flex flex-col justify-between  pl-10 pt-8 grow text-black'>
+      <header className='text-tittle-buttons text-[24px] mb-4 mt-3'>Buttons</header>
 
-      <aricle className='flex flex-row w-[300px] justify-between text-[10px] mb-10' >
+      <aricle className='flex flex-row w-[300px] justify-between text-[10px]' >
         <div>
           <div> &lt;Default /&gt; </div>
           <button class="bg-gray-200 font-bold py-2 px-4 rounded mt-2">Default</button>
@@ -26,11 +26,103 @@ function Buttons() {
           <button class="bg-blue-100 focus:ring focus:ring-blue hover:bg-blue-300 border border-blue-500 text-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
         </div>
       </aricle>
- 
 
-      <article className='m-4 text-footer font-semibold text-[12px]'>Icons: <a href='https://material.io/resources/icons/?style=round'>https://material.io/resources/icons/?style=round</a></article> 
+      <aricle className='flex flex-row w-[300px] justify-between text-[10px]' >
+        <div>
+          <div> &lt;Button variant="text"/&gt; </div>
+          <button class="bg-transparent text-blue-500 border border-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+        <div>
+          <div className='text-gray-400'> &:hover, &:focus </div>
+          <button class="bg-blue-100 focus:ring focus:ring-blue hover:bg-blue-300 border border-blue-500 text-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+      </aricle>
+
+      <aricle className='flex flex-row w-[300px] justify-between text-[10px]' >
+        <div>
+          <div> &lt;Button disableShadow"/&gt; </div>
+          <button class="bg-transparent text-blue-500 border border-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+      </aricle>
+
+      <aricle className='flex flex-row w-[378px] justify-between text-[10px]' >
+        <div>
+          <div> &lt;Button disabled"/&gt; </div>
+          <button class="bg-transparent text-blue-500 border border-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+        <div>
+          <div className='text-gray-400'> &lt;Button variant="text" disabled/&gt; </div>
+          <button class="bg-blue-100 focus:ring focus:ring-blue hover:bg-blue-300 border border-blue-500 text-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+      </aricle>
+
+      <aricle className='flex flex-row w-[450px] justify-between text-[10px]' >
+        <div>
+          <div> &lt;Button startIcon="local_grocery_store"/&gt; </div>
+          <button class="bg-transparent text-blue-500 border border-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+        <div>
+          <div className='text-gray-400'> &lt;Button endIcon="local_grocery_store"/&gt; </div>
+          <button class="bg-blue-100 focus:ring focus:ring-blue hover:bg-blue-300 border border-blue-500 text-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+      </aricle>
+ 
+      <aricle className='flex flex-row w-[400px] justify-between text-[10px]' >
+        <div>
+          <div> &lt;Button size="sm"/&gt; </div>
+          <button class="bg-transparent text-blue-500 border border-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+        <div>
+          <div className='text-gray-400'> &lt;Button size="md"/&gt; </div>
+          <button class="bg-blue-100 focus:ring focus:ring-blue hover:bg-blue-300 border border-blue-500 text-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+        <div>
+          <div className='text-gray-400'> &lt;Button size="lg"/&gt; </div>
+          <button class="bg-blue-100 focus:ring focus:ring-blue hover:bg-blue-300 border border-blue-500 text-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+      </aricle>
+
+      <aricle className='flex flex-row w-[600px] justify-between text-[10px]' >
+        <div>
+          <div> &lt;Button color="default"/&gt; </div>
+          <button class="bg-transparent text-blue-500 border border-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+        <div>
+          <div className='text-gray-400'> &lt;Button color="primary"/&gt; </div>
+          <button class="bg-blue-100 focus:ring focus:ring-blue hover:bg-blue-300 border border-blue-500 text-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+        <div>
+          <div className='text-gray-400'>  &lt;Button color="secondary"/&gt; </div>
+          <button class="bg-blue-100 focus:ring focus:ring-blue hover:bg-blue-300 border border-blue-500 text-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+        <div>
+          <div className='text-gray-400'>  &lt;Button color="danger"/&gt; </div>
+          <button class="bg-blue-100 focus:ring focus:ring-blue hover:bg-blue-300 border border-blue-500 text-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+      </aricle>
+
+      <aricle className='flex flex-row w-[600px] justify-between text-[10px]' >
+        <div>
+          <div> &lt;Button color="default"/&gt; </div>
+          <button class="bg-transparent text-blue-500 border border-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+        <div>
+          <div className='text-gray-400'> &lt;Button color="primary"/&gt; </div>
+          <button class="bg-blue-100 focus:ring focus:ring-blue hover:bg-blue-300 border border-blue-500 text-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+        <div>
+          <div className='text-gray-400'>  &lt;Button color="secondary"/&gt; </div>
+          <button class="bg-blue-100 focus:ring focus:ring-blue hover:bg-blue-300 border border-blue-500 text-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+        <div>
+          <div className='text-gray-400'>  &lt;Button color="danger"/&gt; </div>
+          <button class="bg-blue-100 focus:ring focus:ring-blue hover:bg-blue-300 border border-blue-500 text-blue-500 font-bold py-2 px-4 rounded mt-2">Default</button>
+        </div>
+      </aricle>
+
+      <article className='text-footer font-semibold text-[12px]'>Icons: <a href='https://material.io/resources/icons/?style=round'>https://material.io/resources/icons/?style=round</a></article> 
       
-      <footer className='m-4 text-footer font-semibold text-[14px]'>
+      <footer className='text-footer font-semibold text-[14px]'>
         created by <span className='underline font-bold'>Martin Boglione</span> - devChallenges.io
       </footer>
     </section>
