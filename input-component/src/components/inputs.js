a87891698fcbbac2df40c073ccec7aba8c8f2970
Martin <martinboglione97@gmail.com>
2023-09-18 19:45:01 -0300

    Added all inputs lines

--- input-component/src/components/inputs.js
+++ input-component/src/components/inputs.js
@@ -17,12 +17,101 @@ function Inputs() {
           <div className="flex flex-col">
             <div className='text-gray-400'> &:hover</div>
             <label className="text-gray-900 mt-2 ">Label</label>
-            <input type="text" className="border border-gray-800 text-gray-900 text-sm font-semibold rounded-lg p-2 h-12 w-42" placeholder="Placeholder"></input>
+            <input type="text" className="border border-gray-800 text-gray-900 text-sm font-semibold hover:bg-gray-200 rounded-lg p-2 h-12 w-42" placeholder="Placeholder"></input>
           </div>
           <div className="flex flex-col">
             <div className='text-gray-400'> &:focus </div>
             <label className="text-blue-900 mt-2">Label</label>
-            <input type="text" className="border border-blue-700 text-gray-900 text-sm placeholder-gray-400 font-semibold rounded-lg p-2 h-12 w-42" placeholder="Placeholder"></input>
+            <input type="text" className="border border-blue-700 text-gray-900 text-sm placeholder-gray-400 font-semibold focus:ring-2 focus:outline-none focus:ring-blue-900 rounded-lg p-2 h-12 w-42" placeholder="Placeholder"></input>
+          </div>
+        </aricle>
+
+        <aricle className='flex flex-row w-[728px] h-[105px] justify-between text-[10px] mb-2' >
+          <div className="flex flex-col">
+            <div> &lt;Input /&gt; </div>
+            <label className="text-gray-900 mt-2">Label</label>
+            <input type="text" className="border border-gray-400 text-gray-900 text-sm font-semibold rounded-lg p-2 h-12 w-42" placeholder="Placeholder"></input>
+          </div>
+          <div className="flex flex-col">
+            <div className='text-gray-400'> &:hover</div>
+            <label className="text-gray-900 mt-2 ">Label</label>
+            <input type="text" className="border border-gray-800 text-gray-900 text-sm font-semibold hover:bg-gray-200 rounded-lg p-2 h-12 w-42" placeholder="Placeholder"></input>
+          </div>
+          <div className="flex flex-col">
+            <div className='text-gray-400'> &:focus </div>
+            <label className="text-blue-900 mt-2">Label</label>
+            <input type="text" className="border border-blue-700 text-gray-900 text-sm placeholder-gray-400 font-semibold focus:ring-2 focus:outline-none focus:ring-blue-900 rounded-lg p-2 h-12 w-42" placeholder="Placeholder"></input>
+          </div>
+        </aricle>
+
+        <aricle className='flex flex-row w-[728px] h-[105px] justify-between text-[10px] mb-2' >
+          <div className="flex flex-col">
+            <div> &lt;Input /&gt; </div>
+            <label className="text-gray-900 mt-2">Label</label>
+            <input type="text" className="border border-gray-400 text-gray-900 text-sm font-semibold rounded-lg p-2 h-12 w-42" placeholder="Placeholder"></input>
+          </div>
+        </aricle>
+
+        <aricle className='flex flex-row w-[728px] h-[105px] justify-between text-[10px] mb-2' >
+          <div className="flex flex-col">
+            <div> &lt;Input /&gt; </div>
+            <label className="text-gray-900 mt-2">Label</label>
+            <input type="text" className="border border-gray-400 text-gray-900 text-sm font-semibold rounded-lg p-2 h-12 w-42" placeholder="Placeholder"></input>
+          </div>
+          <div className="flex flex-col">
+            <div className='text-gray-400'> &:hover</div>
+            <label className="text-gray-900 mt-2 ">Label</label>
+            <input type="text" className="border border-gray-800 text-gray-900 text-sm font-semibold hover:bg-gray-200 rounded-lg p-2 h-12 w-42" placeholder="Placeholder"></input>
+          </div>
+        </aricle>
+
+        <aricle className='flex flex-row w-[728px] h-[105px] justify-between text-[10px] mb-2' >
+          <div className="flex flex-col">
+            <div> &lt;Input /&gt; </div>
+            <label className="text-gray-900 mt-2">Label</label>
+            <input type="text" className="border border-gray-400 text-gray-900 text-sm font-semibold rounded-lg p-2 h-12 w-42" placeholder="Placeholder"></input>
+          </div>
+          <div className="flex flex-col">
+            <div className='text-gray-400'> &:hover</div>
+            <label className="text-gray-900 mt-2 ">Label</label>
+            <input type="text" className="border border-gray-800 text-gray-900 text-sm font-semibold hover:bg-gray-200 rounded-lg p-2 h-12 w-42" placeholder="Placeholder"></input>
+          </div>
+        </aricle>
+
+        <aricle className='flex flex-row w-[728px] h-[105px] justify-between text-[10px] mb-2' >
+          <div className="flex flex-col">
+            <div> &lt;Input /&gt; </div>
+            <label className="text-gray-900 mt-2">Label</label>
+            <input type="text" className="border border-gray-400 text-gray-900 text-sm font-semibold rounded-lg p-2 h-12 w-42" placeholder="Placeholder"></input>
+          </div>
+        </aricle>
+
+        <aricle className='flex flex-row w-[728px] h-[105px] justify-between text-[10px] mb-2' >
+          <div className="flex flex-col">
+            <div> &lt;Input /&gt; </div>
+            <label className="text-gray-900 mt-2">Label</label>
+            <input type="text" className="border border-gray-400 text-gray-900 text-sm font-semibold rounded-lg p-2 h-12 w-42" placeholder="Placeholder"></input>
+          </div>
+          <div className="flex flex-col">
+            <div className='text-gray-400'> &:hover</div>
+            <label className="text-gray-900 mt-2 ">Label</label>
+            <input type="text" className="border border-gray-800 text-gray-900 text-sm font-semibold hover:bg-gray-200 rounded-lg p-2 h-12 w-42" placeholder="Placeholder"></input>
+          </div>
+        </aricle>
+
+        <aricle className='flex flex-row w-[728px] h-[105px] justify-between text-[10px] mb-2' >
+          <div className="flex flex-col">
+            <div> &lt;Input /&gt; </div>
+            <label className="text-gray-900 mt-2">Label</label>
+            <input type="text" className="border border-gray-400 text-gray-900 text-sm font-semibold rounded-lg p-2 h-12 w-42" placeholder="Placeholder"></input>
+          </div>
+        </aricle>
+
+        <aricle className='flex flex-row w-[728px] h-[105px] justify-between text-[10px] mb-2' >
+          <div className="flex flex-col">
+            <div> &lt;Input /&gt; </div>
+            <label className="text-gray-900 mt-2">Label</label>
+            <input type="text" className="border border-gray-400 text-gray-900 text-sm font-semibold rounded-lg p-2 h-12 w-42" placeholder="Placeholder"></input>
           </div>
         </aricle>
         
